4622e867bdd1ed61f436c9575b8b2d09be1a7c6f
Theodor Schaathun <thesch@gvgs.no>
2026-04-29 15:19:42 +0200

    la til mer i script.js

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ let green = 120
 let blue = 120
 
 let title = document.querySelector("h2")
+title.addEventListener("click", function () {
+    document.body.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")"
+})
